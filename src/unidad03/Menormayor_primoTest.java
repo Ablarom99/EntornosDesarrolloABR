27ef542f0065b0064d0e11a804d1d6f1b539defe
Ablarom99 <71704306+Ablarom99@users.noreply.github.com>
2020-12-09 14:52:22 +0100

    add

---
+++ src/unidad03/Menormayor_primoTest.java
@@ -0,0 +1,27 @@
+package unidad03;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class menormayor_primoTest {
+
+	@Test
+	public void test() {
+		
+		int numero = 12;	
+		int resultado[] = new int[] {5,7,11};
+		
+		int primos[] = Menormayor_primo.primos(numero);
+		assertArrayEquals(resultado, primos);
+	}
+	
+	@Test
+	public void testMayorMenor() {
+		
+		int resultado[] = Menormayor_primo.mayorMenor(1, 2, 3);
+		int esperado[] = new int[] {3,1};
+		
+		assertArrayEquals(resultado, esperado);
+	}
+}
